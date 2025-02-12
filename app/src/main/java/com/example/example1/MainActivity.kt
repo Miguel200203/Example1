363cae45146e0df5cfa161f6dfc80b07318ba142
Miguel <22150687@aguascalientes.tecnm.mx>
2025-02-12 07:51:55 -0600

    Box layout and Text properties

--- app/src/main/java/com/example/example1/MainActivity.kt
+++ app/src/main/java/com/example/example1/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
@@ -21,9 +22,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.example1.ui.theme.Example1Theme
 
 class MainActivity : ComponentActivity() {
@@ -42,6 +50,8 @@ class MainActivity : ComponentActivity() {
                     }
                     Column(){
                         modifierExample2()
+                        modifierExample4()
+                        Customtext()
                     }
                 }
 
@@ -106,5 +116,53 @@ class MainActivity : ComponentActivity() {
             TextComposable(" 4")
         }
     }
+    @Preview(showBackground = true)
+    @Composable
+    fun modifierExample4(){
+        Box(
+            modifier = Modifier
+                .padding(10.dp)
+                .width(300.dp)
+                .height(300.dp)
+                .background(Color.Gray)
+
+        ){
+            Text("1", Modifier.align(Alignment.TopStart))
+            Text("2", Modifier.align(Alignment.TopCenter))
+            Text("3", Modifier.align(Alignment.TopEnd))
+            Text("4", Modifier.align(Alignment.CenterStart))
+            Text("5", Modifier.align(Alignment.Center))
+            Text("6", Modifier.align(Alignment.CenterEnd))
+            Text("7", Modifier.align(Alignment.BottomStart))
+            Text("8", Modifier.align(Alignment.BottomCenter))
+            Text("9", Modifier.align(Alignment.BottomEnd))
+
+        }
+
+
+        }
+    @Preview(showBackground = true)
+    @Composable
+    fun Customtext(){
+        Column (){
+            Text(
+                stringResource(R.string.example_text),
+                color = colorResource(R.color.purple_700),
+                fontSize = 20.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.W700
+            )
+            val gradientcolors = listOf(Color.Cyan,Color.DarkGray, colorResource(R.color.purple_700))
+            Text(
+                stringResource(R.string.example_text),
+                style = TextStyle(brush = Brush.linearGradient(colors = gradientcolors ))
+            )
+        }
+
+    }
+
+
+
+
 }
 
