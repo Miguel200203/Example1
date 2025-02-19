029da95ac174b1024770fdd329b35c8daaa75f8a
Miguel <22150687@aguascalientes.tecnm.mx>
2025-02-19 08:51:59 -0600

    Se agrego la interfaz de StockApp y se agrego un boton para abrirlo

--- app/src/main/java/com/example/example1/ui/Screens/MainMenuScreen.kt
+++ app/src/main/java/com/example/example1/ui/Screens/MainMenuScreen.kt
@@ -1,36 +1,58 @@
 package com.rick.workclass.ejemplo.com.example.example1.ui.Screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.example1.R
 
 @Composable
 fun MainMenuScreen(navHostController: NavHostController){
     Column (
         modifier = Modifier
             .padding(10.dp)
-            .fillMaxSize()
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
 
 
     ){
-        Text("Main Menu")
+        Text("Main Menu",
+            fontSize = 25.sp)
         Button(
-            onClick = {navHostController.navigate("Home_Screen") }
+            onClick = {navHostController.navigate("Home_Screen")
+                 }
 
         ) {
-            Text("Go to Home Screen")
+            Text("Go to Home Screen",
+                fontSize = 20.sp)
         }
         Button(
             onClick = {navHostController.navigate("test_screen") }
         ) {
-            Text("Go to Test Screen")
+            Text("Go to Test Screen",
+                fontSize = 20.sp)
+        }
+        Button(
+            onClick = {navHostController.navigate("StockApp") }
+
+        ) {
+
+            Text(text="Go to App Stock",
+               fontSize = 20.sp)
         }
     }
 }
